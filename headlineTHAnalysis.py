dffd23a07befdb4b8ccf4e92ef96222b2ed927a6
Sania <sania.d@myplanet.com>
2019-12-01 23:30:47 -0500

    Penultimate changes to the web scraper

--- headlineTHAnalysis.py
+++ headlineTHAnalysis.py
@@ -6,18 +6,25 @@ import requests
 from bs4 import BeautifulSoup as bs
 from ttictoc import TicToc
 import re, nltk
+import matplotlib.pyplot as plt
+import seaborn as sns
 #from nltk.stem import PorterStemmer 
 #nltk.download('stopwords')     # needs to be done once
-
 from helperFunctions import dataCollectorFunctions as dc
+from collections import Counter
+
+# initialise a dictionary that will hold the yearly data as elements
+final_YearlyData = {}
 
 # get the data
 # source : The Hindu, for example, the format of the page link: https://www.thehindu.com/archive/web/2009/08/18/
 startYear = 2010
-lastYear = 2010
+lastYear = 2011
+
 #ps = PorterStemmer()
 for year in range(startYear, lastYear+1):
 
+    words_final = []   #reinitialize the yearly data list 
     # get the data to analyze
     t = TicToc()
     t.tic()
@@ -44,7 +51,29 @@ for year in range(startYear, lastYear+1):
     for word in words:
         if word not in sw:
             words_final.append(word)
-    print(words_final)
-    # evaluate the data
+    
+    final_YearlyData[year] = words_final
 
-    # 
+    
+# evaluate the data
+# get the most frequently-used words and plot the word vs count graph
+# get the values of the dictionary final_YearlyData as separate values in this list
+
+dict_Values = [final_YearlyData[i] for i in final_YearlyData.keys()]
+for i in dict_Values:
+    mostFreq = Counter(i).most_common(1)
+    print(mostFreq)
+
+
+# changes to be made on 12/02
+# plot the year vs the counter of the most-used word (probably a function to calculate this)
+
+
+
+
+# This is not the required solution here
+# for i in range(0,(lastYear-startYear)):
+#     freqdist2 = nltk.FreqDist(dict_Values[i])
+#     freqdist1.append(freqdist2[1])  # get the top-most important word in a year's headlines and compare with the rest of the years
+
+# freqdist1.plot()
